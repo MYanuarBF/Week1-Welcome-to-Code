b053b9b1cae3030a67d3867247437b52851c42a2
MYanuarBF <beep4685@gmail.com>
2025-03-09 16:26:09 +0700

    mengerjakan quiz part2

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,16 @@
+// Problem
+// Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+
+console.log('First Word: ' + exampleFirstWord3);
+
+let second = word3.substring(4, 14);
+console.log("Second Word: " +second);
+let third = word3.substring(15, 17);
+console.log("Third Word: " +third);
+let fourth = word3.substring(18, 20);
+console.log("Fourth Word: " +fourth);
+let fifth = word3.substring(21, 25);
+console.log("Fifth Word: " +fifth);
---
+++ quiz/part2/soal4.js
@@ -0,0 +1,20 @@
+// Problem
+// Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0, 3);
+
+let firstWordLength = exampleFirstWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+
+
+let second = word4.substring(4, 14);
+console.log("Second Word: " +second + ', with length: ' + second.length);
+let third = word4.substring(15, 17);
+console.log("Third Word: " +third + ', with length: ' + third.length);
+let fourth = word4.substring(18, 20);
+console.log("Fourth Word: " +fourth + ', with length: ' + fourth.length);
+let fifth = word4.substring(21, 25);
+console.log("Fifth Word: " +fifth + ', with length: ' + fifth.length);
